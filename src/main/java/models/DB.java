83d9916926ac6f5fa2a26b9d8f3dd0a83faf50b8
lifereze <liferezearnold@gmail.com>
2020-07-19 02:12:13 +0300

    initial commit

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -15,15 +15,15 @@ public class DB {
 
         try {
             if (System.getenv("DATABASE_URL") == null) {
-                dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
+                dbUri = new URI("postgres://localhost:5432/Wildlife-Tracker");
             } else {
                 dbUri = new URI(System.getenv("DATABASE_URL"));
             }
             int port = dbUri.getPort();
             String host = dbUri.getHost();
             String path = dbUri.getPath();
-            String username = (dbUri.getUserInfo() == null) ? "Lifereze Arnold" : dbUri.getUserInfo().split(":")[0];
-            String password = (dbUri.getUserInfo() == null) ? "Access" : dbUri.getUserInfo().split(":")[1];
+            String username = (dbUri.getUserInfo() == null) ? "moringa" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "lifereze1998" : dbUri.getUserInfo().split(":")[1];
             sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
         } catch (URISyntaxException e ) {
             logger.error("Unable to connect to database.");
