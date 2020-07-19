6cf01bad730327a48bcd3eb8ec1f7680e7ba2189
lifereze <liferezearnold@gmail.com>
2020-07-19 03:50:39 +0300

    add functionality to add age and health

--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -9,7 +9,7 @@ import org.sql2o.Sql2o;
 public class DatabaseRule extends ExternalResource {
     @Override
     protected void before() throws Throwable {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/Wildlife-Tracker_test","moringa", "lifereze1998");
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test","moringa", "lifereze1998");
     }
 
     @Override
